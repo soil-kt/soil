7bfe9571dd2c62489ffe183e461fc1655e6438d7
ogaclejapan <ogaclejapan@gmail.com>
2024-07-28 11:03:49 +0900

    Add new actor logic

---
+++ soil-query-core/src/commonMain/kotlin/soil/query/internal/Actor.kt
@@ -0,0 +1,93 @@
+// Copyright 2024 Soil Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package soil.query.internal
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.time.Duration
+
+/**
+ * An actor represents a launch entry point for processing a query or mutation command.
+ */
+interface Actor {
+
+    /**
+     * Launches the actor.
+     *
+     * The Actor will continue to run as long as any one of the invoked [scope]s is valid.
+     *
+     * **Note:**
+     * Currently, This function must be called from the main(UI) thread.
+     * If you call it from a thread other than the main thread, the internal counter value will be out of sync.
+     *
+     * @param scope The scope in which the actor will run
+     */
+    fun launchIn(scope: CoroutineScope)
+}
+
+internal typealias ActorSequenceNumber = Int
+
+internal class ActorBlockRunner(
+    private val scope: CoroutineScope,
+    private val options: ActorOptions,
+    private val onTimeout: (ActorSequenceNumber) -> Unit,
+    private val block: suspend () -> Unit
+) : Actor {
+
+    var seq: ActorSequenceNumber = 0
+        private set
+
+    private var launchedCount: Int = 0
+    private var hasActiveScope: Boolean = false
+    private var runningJob: Job? = null
+    private var cancellationJob: Job? = null
+
+    override fun launchIn(scope: CoroutineScope) {
+        seq++
+        scope.launch(start = CoroutineStart.UNDISPATCHED) {
+            cancellationJob?.cancelAndJoin()
+            cancellationJob = null
+            suspendCancellableCoroutine { continuation ->
+                launchedCount++
+                if (!hasActiveScope && launchedCount > 0) {
+                    hasActiveScope = true
+                    start()
+                }
+                continuation.invokeOnCancellation {
+                    launchedCount--
+                    if (hasActiveScope && launchedCount <= 0) {
+                        hasActiveScope = false
+                        stop()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun start() {
+        if (runningJob?.isActive == true) {
+            return
+        }
+        runningJob = scope.launch {
+            block()
+        }
+    }
+
+    private fun stop() {
+        if (cancellationJob?.isActive == true) {
+            return
+        }
+        cancellationJob = scope.launch {
+            if (options.keepAliveTime >= Duration.ZERO) {
+                delay(options.keepAliveTime)
+            }
+            onTimeout(seq)
+        }
+    }
+}
