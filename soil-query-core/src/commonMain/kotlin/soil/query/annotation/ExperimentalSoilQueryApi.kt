14f9c21eacefa703ace64b16b205aeca9aa5c342
ogaclejapan <ogaclejapan@users.noreply.github.com>
2024-08-18 06:25:32 +0000

    Apply automatic changes

--- soil-query-core/src/commonMain/kotlin/soil/query/annotation/ExperimentalSoilQueryApi.kt
+++ soil-query-core/src/commonMain/kotlin/soil/query/annotation/ExperimentalSoilQueryApi.kt
@@ -1,3 +1,6 @@
+// Copyright 2024 Soil Contributors
+// SPDX-License-Identifier: Apache-2.0
+
 package soil.query.annotation
 
 @RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.")
