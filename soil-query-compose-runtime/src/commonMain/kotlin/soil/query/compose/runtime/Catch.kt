903ea9747e952e387056c908f3f4037a0f5e8f26
ogaclejapan <ogaclejapan@gmail.com>
2024-09-17 08:07:31 +0900

    wip

--- soil-query-compose-runtime/src/commonMain/kotlin/soil/query/compose/runtime/Catch.kt
+++ soil-query-compose-runtime/src/commonMain/kotlin/soil/query/compose/runtime/Catch.kt
@@ -11,192 +11,21 @@ import soil.query.core.DataModel
 import soil.query.core.uuid
 
 /**
- * Catch for a [DataModel] to be rejected.
- *
- * @param state The [DataModel] to catch.
- * @param isEnabled Whether to catch the error.
- * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
- */
-@Composable
-fun Catch(
-    state: DataModel<*>,
-    isEnabled: Boolean = true,
-    content: @Composable CatchScope.(err: Throwable) -> Unit = { Throw(error = it) }
-) {
-    Catch(
-        state = state,
-        filterIsInstance = { it },
-        isEnabled = isEnabled,
-        content = content
-    )
-}
-
-/**
- * Catch for any [DataModel]s to be rejected.
- *
- * @param state1 The first [DataModel] to catch.
- * @param state2 The second [DataModel] to catch.
- * @param isEnabled Whether to catch the error.
- * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
- */
-@Composable
-fun Catch(
-    state1: DataModel<*>,
-    state2: DataModel<*>,
-    isEnabled: Boolean = true,
-    content: @Composable CatchScope.(err: Throwable) -> Unit = { Throw(error = it) }
-) {
-    Catch(
-        state1 = state1,
-        state2 = state2,
-        filterIsInstance = { it },
-        isEnabled = isEnabled,
-        content = content
-    )
-}
-
-/**
- * Catch for any [DataModel]s to be rejected.
- *
- * @param state1 The first [DataModel] to catch.
- * @param state2 The second [DataModel] to catch.
- * @param state3 The third [DataModel] to catch.
- * @param isEnabled Whether to catch the error.
- * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
- */
-@Composable
-fun Catch(
-    state1: DataModel<*>,
-    state2: DataModel<*>,
-    state3: DataModel<*>,
-    isEnabled: Boolean = true,
-    content: @Composable CatchScope.(err: Throwable) -> Unit = { Throw(error = it) }
-) {
-    Catch(
-        state1 = state1,
-        state2 = state2,
-        state3 = state3,
-        filterIsInstance = { it },
-        isEnabled = isEnabled,
-        content = content
-    )
-}
-
-/**
  * Catch for any [DataModel]s to be rejected.
  *
  * @param states The [DataModel]s to catch.
- * @param isEnabled Whether to catch the error.
+ * @param filter A function to filter the error.
  * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
  */
 @Composable
-fun Catch(
+inline fun Catch(
     vararg states: DataModel<*>,
-    isEnabled: Boolean = true,
+    filter: (DataModel<*>) -> Boolean = { true },
     content: @Composable CatchScope.(err: Throwable) -> Unit = { Throw(error = it) }
 ) {
-    Catch(
-        states = states,
-        filterIsInstance = { it },
-        isEnabled = isEnabled,
-        content = content
-    )
-}
-
-/**
- * Catch for a [DataModel] to be rejected.
- *
- * @param state The [DataModel] to catch.
- * @param filterIsInstance A function to filter the error.
- * @param isEnabled Whether to catch the error.
- * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
- */
-@Composable
-fun <T : Throwable> Catch(
-    state: DataModel<*>,
-    filterIsInstance: (err: Throwable) -> T?,
-    isEnabled: Boolean = true,
-    content: @Composable CatchScope.(err: T) -> Unit = { Throw(error = it) }
-) {
-    val err = state.error.takeIf { isEnabled }?.let(filterIsInstance)
-    if (err != null) {
-        with(CatchScope) { content(err) }
-    }
-}
-
-/**
- * Catch for any [DataModel]s to be rejected.
- *
- * @param state1 The first [DataModel] to catch.
- * @param state2 The second [DataModel] to catch.
- * @param filterIsInstance A function to filter the error.
- * @param isEnabled Whether to catch the error.
- * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
- */
-@Composable
-fun <T : Throwable> Catch(
-    state1: DataModel<*>,
-    state2: DataModel<*>,
-    filterIsInstance: (err: Throwable) -> T?,
-    isEnabled: Boolean = true,
-    content: @Composable CatchScope.(err: T) -> Unit = { Throw(error = it) }
-) {
-    val err = listOf(state1, state2).takeIf { isEnabled }
-        ?.firstNotNullOfOrNull { it.error }
-        ?.let(filterIsInstance)
-
-    if (err != null) {
-        with(CatchScope) { content(err) }
-    }
-}
-
-/**
- * Catch for any [DataModel]s to be rejected.
- *
- * @param state1 The first [DataModel] to catch.
- * @param state2 The second [DataModel] to catch.
- * @param state3 The third [DataModel] to catch.
- * @param filterIsInstance A function to filter the error.
- * @param isEnabled Whether to catch the error.
- * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
- */
-@Composable
-fun <T : Throwable> Catch(
-    state1: DataModel<*>,
-    state2: DataModel<*>,
-    state3: DataModel<*>,
-    filterIsInstance: (err: Throwable) -> T?,
-    isEnabled: Boolean = true,
-    content: @Composable CatchScope.(err: T) -> Unit = { Throw(error = it) }
-) {
-    val err = listOf(state1, state2, state3).takeIf { isEnabled }
-        ?.firstNotNullOfOrNull { it.error }
-        ?.let(filterIsInstance)
-
-    if (err != null) {
-        with(CatchScope) { content(err) }
-    }
-}
-
-/**
- * Catch for any [DataModel]s to be rejected.
- *
- * @param states The [DataModel]s to catch.
- * @param filterIsInstance A function to filter the error.
- * @param isEnabled Whether to catch the error.
- * @param content The content to display when the query is rejected. By default, it [throws][CatchScope.Throw] the error.
- */
-@Composable
-fun <T : Throwable> Catch(
-    vararg states: DataModel<*>,
-    filterIsInstance: (err: Throwable) -> T?,
-    isEnabled: Boolean = true,
-    content: @Composable CatchScope.(err: T) -> Unit = { Throw(error = it) }
-) {
-    val err = states.takeIf { isEnabled }
-        ?.firstNotNullOfOrNull { it.error }
-        ?.let(filterIsInstance)
-
+    val err = states
+        .filter { it.error != null && filter(it) }
+        .minByOrNull { it.errorUpdatedAt }?.error
     if (err != null) {
         with(CatchScope) { content(err) }
     }
