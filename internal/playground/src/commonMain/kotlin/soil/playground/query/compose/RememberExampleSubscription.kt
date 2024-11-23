4bd7b0a819d9f85f8d06374b63e30ca4679ff262
ogaclejapan <ogaclejapan@gmail.com>
2024-11-24 08:56:57 +0900

    Add Namespace Extension Property `autoCompositionKeyHash`
    In #115 and #116, we introduced the `auto` extension function to assist with the automatic generation of unique namespaces for Mutations and Subscriptions.
    This PR adds an alternative that allows the use of a value calculated from `currentCompositionKeyHash` instead of the
    `auto` function, but only within a Composable function limited to a single key.

--- internal/playground/src/commonMain/kotlin/soil/playground/query/compose/RememberExampleSubscription.kt
+++ internal/playground/src/commonMain/kotlin/soil/playground/query/compose/RememberExampleSubscription.kt
@@ -5,13 +5,13 @@ import soil.playground.query.key.ExampleSubscriptionKey
 import soil.query.annotation.ExperimentalSoilQueryApi
 import soil.query.compose.SubscriptionObject
 import soil.query.compose.rememberSubscription
-import soil.query.compose.util.auto
+import soil.query.compose.util.autoCompositionKeyHash
 import soil.query.core.Namespace
 
 @OptIn(ExperimentalSoilQueryApi::class)
 @Composable
 fun rememberExampleSubscription(): SubscriptionObject<String> {
     return rememberSubscription(
-        key = ExampleSubscriptionKey(Namespace.auto())
+        key = ExampleSubscriptionKey(Namespace.autoCompositionKeyHash)
     )
 }
