f1bebe23093c10d3e68b6d884db446c326a31c83
ogaclejapan <ogaclejapan@gmail.com>
2025-04-03 08:52:51 +0900

    fixup! Apply automatic changes

--- soil-query-compose/src/commonMain/kotlin/soil/query/compose/util/OptimisticUpdate.kt
+++ soil-query-compose/src/commonMain/kotlin/soil/query/compose/util/OptimisticUpdate.kt
@@ -1,6 +1,8 @@
 // Copyright 2025 Soil Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+@file:Suppress("NOTHING_TO_INLINE", "KotlinRedundantDiagnosticSuppress")
+
 package soil.query.compose.util
 
 import androidx.compose.runtime.Composable
@@ -9,64 +11,61 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DisposableHandle
 import kotlinx.coroutines.Job
 
+typealias OptimisticUpdate<T, D> = (currentState: T, optimisticValue: D) -> T
+typealias OptimisticCompletionHandle = (cause: Throwable?) -> Unit
+
 @Composable
-fun <T> rememberOptimistic(
-    scope: CoroutineScope = rememberCoroutineScope(),
+inline fun <T> rememberOptimistic(
     state: T,
     policy: OptimisticUpdatePolicy = OptimisticUpdatePolicy.Default,
 ): OptimisticObject<T, T> {
-
-    val passThrough = remember<OptimisticUpdateFn<T, T>> {
-        OptimisticUpdateFn { _, optimisticValue ->
-            optimisticValue
-        }
-    }
-
     return rememberOptimistic(
-        scope = scope,
         state = state,
-        updateFn = passThrough,
+        updateFn = { _, optimisticValue -> optimisticValue },
         policy = policy
     )
 }
 
 @Composable
 fun <T, D> rememberOptimistic(
-    scope: CoroutineScope = rememberCoroutineScope(),
     state: T,
-    updateFn: OptimisticUpdateFn<T, D>,
     policy: OptimisticUpdatePolicy = OptimisticUpdatePolicy.Default,
+    updateFn: OptimisticUpdate<T, D>
 ): OptimisticObject<T, D> {
 
-    var pendingValues by remember { mutableStateOf(emptyList<Pair<D, Job>>()) }
-    val optimisticState = remember(pendingValues) {
-        pendingValues.fold(state) { acc, (optimisticValue, _) ->
+    var pendingUpdates by remember { mutableStateOf(emptyList<Triple<D, Job, DisposableHandle>>()) }
+    val optimisticState = remember(state, pendingUpdates) {
+        pendingUpdates.fold(state) { acc, (optimisticValue, _, _) ->
             updateFn(acc, optimisticValue)
         }
     }
 
-    val addOptimistic = remember<CoroutineScope.(D) -> Unit> {
+    val addOptimistic = remember<CoroutineScope.(D) -> OptimisticCompletionHandle> {
         { value ->
             val currentJob = coroutineContext[Job] ?: error("No Job in CoroutineScope")
-            pendingValues += Pair(value, currentJob)
-            currentJob.invokeOnCompletion { err ->
-                pendingValues = if (err != null && policy.shouldResetOnError(cause = err)) {
-                    emptyList()
+            val completionHandle: OptimisticCompletionHandle = { err ->
+                if (err != null && policy.shouldResetOnError(cause = err)) {
+                    pendingUpdates.forEach { (_, job, handle) ->
+                        handle.dispose()
+                        job.cancel()
+                    }
+                    pendingUpdates = emptyList()
                 } else {
-                    pendingValues.filterNot { (_, job) -> job === currentJob }
+                    pendingUpdates = pendingUpdates.filterNot { (_, job, _) -> job === currentJob }
                 }
             }
+            val disposableHandle = currentJob.invokeOnCompletion { err -> completionHandle(err) }
+            pendingUpdates += Triple(value, currentJob, disposableHandle)
+            completionHandle
         }
     }
 
     return OptimisticObject(
-        scope = scope,
         state = optimisticState,
         add = addOptimistic,
     )
@@ -74,28 +73,15 @@ fun <T, D> rememberOptimistic(
 
 @Immutable
 data class OptimisticObject<T, D>(
-    val scope: CoroutineScope,
     val state: T,
-    val add: CoroutineScope.(D) -> Unit,
+    val add: CoroutineScope.(D) -> OptimisticCompletionHandle,
 )
-
-@Stable
-fun interface OptimisticUpdateFn<T, D> {
-    operator fun invoke(currentState: T, optimisticValue: D): T
-}
 
 @Stable
 interface OptimisticUpdatePolicy {
     fun shouldResetOnError(cause: Throwable): Boolean
 
-    companion object
-}
-
-val OptimisticUpdatePolicy.Companion.Default: OptimisticUpdatePolicy
-    get() = DefaultOptimisticUpdatePolicy
-
-private object DefaultOptimisticUpdatePolicy : OptimisticUpdatePolicy {
-    override fun shouldResetOnError(cause: Throwable): Boolean {
-        return cause is CancellationException
+    companion object Default : OptimisticUpdatePolicy {
+        override fun shouldResetOnError(cause: Throwable): Boolean = true
     }
 }
