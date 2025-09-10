7715459e000ecadf9e89de4b3c652f72949287cd
ogaclejapan <ogaclejapan@gmail.com>
2025-09-11 07:40:35 +0900

    wip

--- soil-query-core/src/iosMain/kotlin/soil/query/IosNetworkConnectivity.kt
+++ soil-query-core/src/iosMain/kotlin/soil/query/IosNetworkConnectivity.kt
@@ -3,4 +3,50 @@
 
 package soil.query
 
-// TODO: NWPathMonitor isn't included in the prebuilt libraries for Kotlin Native.
+import platform.Network.*
+import platform.darwin.dispatch_get_main_queue
+import soil.query.core.NetworkConnectivity
+import soil.query.core.NetworkConnectivityEvent
+import kotlinx.cinterop.*
+
+/**
+ * Implementation of [NetworkConnectivity] for iOS using NWPathMonitor.
+ */
+class IosNetworkConnectivity : NetworkConnectivity {
+
+    private var monitor: nw_path_monitor_t? = null
+    private var currentObserver: NetworkConnectivity.Observer? = null
+
+    override fun addObserver(observer: NetworkConnectivity.Observer) {
+        removeObserver(observer)
+        currentObserver = observer
+        monitor = nw_path_monitor_create()
+
+        monitor?.let { pathMonitor ->
+            nw_path_monitor_set_queue(pathMonitor, dispatch_get_main_queue())
+            nw_path_monitor_set_update_handler(pathMonitor) { path ->
+                if (path != null) {
+                    when (nw_path_get_status(path)) {
+                        nw_path_status_satisfied -> {
+                            observer.onReceive(NetworkConnectivityEvent.Available)
+                        }
+                        nw_path_status_unsatisfied -> {
+                            observer.onReceive(NetworkConnectivityEvent.Lost)
+                        }
+                        nw_path_status_satisfiable,
+                        nw_path_status_invalid -> Unit
+                    }
+                }
+            }
+            nw_path_monitor_start(pathMonitor)
+        }
+    }
+
+    override fun removeObserver(observer: NetworkConnectivity.Observer) {
+        monitor?.let { pathMonitor ->
+            nw_path_monitor_cancel(pathMonitor)
+        }
+        monitor = null
+        currentObserver = null
+    }
+}
