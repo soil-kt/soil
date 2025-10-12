d302e67ec4af990a765850bb57ae29d4527966c4
easyhooon <mraz3068@gmail.com>
2025-10-13 02:23:57 +0900

    Refactor sample app to use type-safe Compose Navigation
    Replace string-based navigation with Kotlin serialization-based type-safe navigation API.

--- sample/composeApp/src/commonMain/kotlin/Navigator.kt
+++ sample/composeApp/src/commonMain/kotlin/Navigator.kt
@@ -1,15 +1,11 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import androidx.savedstate.read
+import androidx.navigation.toRoute
 import soil.kmp.screen.HelloFormScreen
 import soil.kmp.screen.HelloQueryDetailScreen
 import soil.kmp.screen.HelloQueryScreen
@@ -30,13 +26,7 @@ class Navigator(
     }
 
     override fun <T : NavRoute> push(route: T) {
-        when (val screen = route as NavScreen) {
-            is NavScreen.Home -> push(NavScreenDestination.Home())
-            is NavScreen.HelloQuery -> push(NavScreenDestination.HelloQuery())
-            is NavScreen.HelloQueryDetail -> push(NavScreenDestination.HelloQueryDetail(screen.postId))
-            is NavScreen.HelloForm -> push(NavScreenDestination.HelloForm())
-            is NavScreen.HelloSpace -> push(NavScreenDestination.HelloSpace())
-        }
+        navController.navigate(route)
     }
 
     override fun back(): Boolean {
@@ -53,39 +43,27 @@ fun NavRouterHost(
     navigator: Navigator,
     modifier: Modifier
 ) {
-    val startDestination = remember(NavScreen.root) { NavScreen.root.destination.route }
     CompositionLocalProvider(LocalNavRouter provides navigator) {
         NavHost(
             navController = navigator.navController,
-            startDestination = startDestination,
+            startDestination = NavScreen.root,
             modifier = modifier
         ) {
-            composable(
-                route = NavScreenDestination.Home.route
-            ) {
+            composable<NavScreen.Home> {
                 HomeScreen()
             }
-            composable(
-                route = NavScreenDestination.HelloQuery.route
-            ) {
+            composable<NavScreen.HelloQuery> {
                 HelloQueryScreen()
             }
-            composable(
-                route = NavScreenDestination.HelloQueryDetail.route,
-                arguments = NavScreenDestination.HelloQueryDetail.arguments
-            ) {
-                val id = checkNotNull(it.arguments?.read { getInt(NavScreenDestination.HelloQueryDetail.id.name) })
-                HelloQueryDetailScreen(postId = id)
+            composable<NavScreen.HelloQueryDetail> {
+                val screen = it.toRoute<NavScreen.HelloQueryDetail>()
+                HelloQueryDetailScreen(postId = screen.postId)
             }
-            composable(
-                route = NavScreenDestination.HelloForm.route
-            ) {
+            composable<NavScreen.HelloForm> {
                 HelloFormScreen()
             }
-            composable(
-                route = NavScreenDestination.HelloSpace.route
-            ) {
-                val rootEntry = navigator.navController.getBackStackEntry(startDestination)
+            composable<NavScreen.HelloSpace> {
+                val rootEntry = navigator.navController.getBackStackEntry<NavScreen.Home>()
                 HelloSpaceScreen(
                     navStore = rememberViewModelStore(rootEntry)
                 )
@@ -93,42 +71,3 @@ fun NavRouterHost(
         }
     }
 }
-
-private sealed class NavScreenDestination(
-    val route: String
-) {
-    data object Home : NavScreenDestination("/home") {
-        operator fun invoke() = route
-    }
-
-    data object HelloQuery : NavScreenDestination("/helloQuery") {
-        operator fun invoke() = route
-    }
-
-    data object HelloQueryDetail : NavScreenDestination("/helloQuery/{id}") {
-        val arguments get() = listOf(id)
-        val id: NamedNavArgument
-            get() = navArgument("id") {
-                type = NavType.IntType
-            }
-
-        operator fun invoke(postId: Int) = "/helloQuery/$postId"
-    }
-
-    data object HelloForm : NavScreenDestination("/helloForm") {
-        operator fun invoke() = route
-    }
-
-    data object HelloSpace : NavScreenDestination("/helloSpace") {
-        operator fun invoke() = route
-    }
-}
-
-private val NavScreen.destination: NavScreenDestination
-    get() = when (this) {
-        is NavScreen.Home -> NavScreenDestination.Home
-        is NavScreen.HelloQuery -> NavScreenDestination.HelloQuery
-        is NavScreen.HelloQueryDetail -> NavScreenDestination.HelloQueryDetail
-        is NavScreen.HelloForm -> NavScreenDestination.HelloForm
-        is NavScreen.HelloSpace -> NavScreenDestination.HelloSpace
-    }
